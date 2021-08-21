d667973fb362ddd352166af6f12a99699fe218ca
Andrew Molina <rasend20@gmail.com>
2021-08-20 17:41:12 -0700

    Logramos scrapear todos los links de la cuenta con exito

--- fuck_instagram.py
+++ fuck_instagram.py
@@ -21,6 +21,13 @@ class iHateInstagram(unittest.TestCase):
 
     def test_instagram(self):
         driver = self.driver 
+
+        def scroll_down(times,sec=1):
+            for i in range(times):
+                #this command helps to scroll down the page
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(sec)
+
         #esperamos a que cargue el boton
         WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//form[@id="loginForm"]//input[@name="username"]')))
         
@@ -41,23 +48,41 @@ class iHateInstagram(unittest.TestCase):
 
         login_btn.click()
         
+        #No nos movemos hasta que cargue por lo menos el elemento de la barra de busqueda
         WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH,'//nav//div/input[@placeholder="Search"]')))
 
+        #Nos movemos al perfil que queremos scrapear
         driver.get(URL_TARGET)
 
         followers = []
 
+        #Encontramos el Numero de posts, los que te siguen y a los que sigues
         for i in range(3):
             info = driver.find_element_by_xpath(f'//header/section/ul/li[{i + 1}]//span').text
             followers.append(info)
 
-        #Extraemos el numero de posts
-        n_posts = int(followers[0][:2])
-        print(n_posts)
-        href = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a')
-        print(href.get_attribute('href'))
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(30)
+
+        #here we wait 1 sec to wait the webelement 
+        scroll_down(1)
+        href = []
+
+        for i in range(8):
+           for y in range(3):
+                link = driver.find_element_by_xpath(f'//article//div[contains(@style,"flex-direction")]/div[{i + 1}]/div[{y + 1}]/a').get_attribute('href')
+                href.append(link)
+        
+        #here we made 3 scroll down and wait 2 sec for the next
+        scroll_down(3,sec=2)
+
+        for i in range(6):
+           for y in range(3):
+                link = driver.find_element_by_xpath(f'//article//div[contains(@style,"flex-direction")]/div[{4 + 1}]/div[{y + 1}]/a').get_attribute('href')
+                href.append(link)
+
+        print(followers)
+        print(href)
+        print(len(href))
+        time.sleep(10)
 
     def tearDown(self):
         self.driver.quit()    
